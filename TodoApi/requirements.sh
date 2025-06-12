17ae4f41ab9a01a882395f5535d071f1653b7fd8
Zafarbek Yunusov <zyunusov30@gmail.com>
2025-06-12 08:28:51 +0500

    add Swagger

---
+++ TodoApi/requirements.sh
@@ -0,0 +1,7 @@
+dotnet add package Microsoft.EntityFrameworkCore
+dotnet add package Microsoft.EntityFrameworkCore.Design
+dotnet add package Npgsql.EntityFrameworkCore.PostgreSQL
+dotnet add package Microsoft.AspNetCore.Authentication.JwtBearer
+dotnet add package FluentValidation
+dotnet add package Serilog.AspNetCore
+dotnet add package Swashbuckle.AspNetCore
